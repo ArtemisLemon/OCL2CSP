3331cc1ade4ddbd4b062aeae132a5c13169f01db
Matthew Coyle <matthew@coyle.fr>
2024-10-11 00:58:20 +0200

    includes and includesAll on constant src

---
+++ app/src/main/java/org/example/Includes.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.chocosolver.solver.*;
+import org.chocosolver.solver.variables.IntVar;
+
+public class Includes {
+    static void includes(Model m, int[] src, IntVar body){
+        m.member(body, src).post();
+    }
+    static void includesAll(Model m, int[] src, IntVar[] body){
+        for(int i=0;i<body.length;i++)
+            includes(m,src,body[i]);
+    }
+    
+    static void includesAll(Model m, int[] src, AdjList body){
+        for(int i=0;i<body.vars().length;i++)
+            includes(m,src,body.vars()[i]);
+    }
+
+}
